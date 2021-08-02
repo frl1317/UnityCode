dd3ea7bc647b53cabab39fb2bd31477c161bff28
frl1317 <frl1317@163.com>
2021-08-02 15:34:59 +0800

    增加保存ui预设 快捷键
    增加保存ui预设 快捷键

--- UnityCode/Assets/编辑器快捷键/Editor/ShortcutTools.cs
+++ UnityCode/Assets/编辑器快捷键/Editor/ShortcutTools.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 
 public class ShortcutTools : Editor
 {
-    [MenuItem("Tools/通用工具/切换物体显隐状态 &q")]
+    [MenuItem("Tools/快捷键/切换选中物体显隐状态 &q")]
     public static void SetObjActive()
     {
         GameObject[] selectObjs = Selection.gameObjects;
@@ -14,6 +14,25 @@ public class ShortcutTools : Editor
         {
             bool isAcitve = selectObjs[i].activeSelf;
             selectObjs[i].SetActive(!isAcitve);
+        }
+    }
+
+    [MenuItem("Tools/快捷键/保存选中ui &s")]
+    public static void SaveSelectPrefab()
+    {
+        GameObject[] selectObjs = Selection.gameObjects;
+        int objCtn = selectObjs.Length;
+        for (int i = 0; i < objCtn; i++)
+        {
+            string prefabPath = UnityEditor.PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(selectObjs[i].transform.root);
+            if(string.IsNullOrEmpty(prefabPath))
+            {
+                Debug.LogWarning("Tools/通用工具/保存选中ui-----选中的物体不是预制体");
+                continue;
+            }
+
+            PrefabUtility.ApplyPrefabInstance(selectObjs[i].transform.root.gameObject, InteractionMode.AutomatedAction);
+            Debug.Log("Tools/通用工具/保存选中ui-----保存预设:"+ prefabPath);
         }
     }
 }
