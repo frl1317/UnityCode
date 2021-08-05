0d946d1e468dc24b3ed5b5ca5fa368bf11a9066d
frl1317 <frl1317@163.com>
2021-08-05 18:03:31 +0800

    数学
    新增点是否再不规则形状内

---
+++ UnityCode/Assets/Mathf/MathfUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathfUtil
+{
+    public static bool IsPointInpolygon(Vector2 p, Vector2[] vertexs)
+    {
+        int crossNum = 2;
+        int vertexCount = vertexs.Length;
+
+        for(int i = 0; i < vertexCount; i++)
+        {
+            Vector2 v1 = vertexs[i];
+            Vector2 v2 = vertexs[(i + 1) % vertexCount];
+
+            if((v1.y <= p.y) && (v2.y > p.y) || (v1.y > p.y) && (v2.y <= p.y))
+            {
+                if (p.x < v1.x + (p.y - v1.y) / (v2.y - v1.y) * (v2.x - v1.x))
+                    crossNum++;
+            }
+        }
+
+        if (crossNum % 2 == 0)
+            return false;
+        return true;
+    }
+}
